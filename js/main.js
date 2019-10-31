bb90a15255c72b97d94cf65617f27d0e4205c442
katemak <km@albedo.dev>
2019-10-31 12:17:58 +0200

    second commit

--- js/main.js
+++ js/main.js
@@ -1,38 +1,5 @@
 $(function() {
-    $("#defaultOpen").get(0).click();
-    //menu toggle
 
-    // $('.menu-toggle').click(function(){
-    //     $('.menu').slideToggle(400)
-    // })
-    //
-    //
-    // $().UItoTop({ easingType: 'easeOutQuart' });
-    //
-    // //smooth scroll to the section
-    //
-    // $("a#apply").click(function() {
-    //     $.scrollTo((".form"));
-    // });
-    //
-    // $("a#services_scroll").click(function() {
-    //     $.scrollTo((".services"));
-    // });
-    //
-    // $("a#about_scroll").click(function() {
-    //     $.scrollTo((".about"));
-    // });
-    //
-    // $("a#advantages_scroll").click(function() {
-    //     $.scrollTo((".advantages"));
-    // });
-    //
-    // $("a#languages_scroll").click(function() {
-    //     $.scrollTo((".languages"));
-    // });
-    //
-    // $("a#contacts_scroll").click(function() {
-    //     $.scrollTo((".contacts"));
-    // });
+    $("#defaultOpen").get(0).click();
 
 });
