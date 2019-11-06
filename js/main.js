91acc5ff6e08c16af7e3f62be89a19587b782695
katemak <km@albedo.dev>
2019-11-06 11:14:30 +0200

    adaptability and header

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
+window.onscroll = function showHeader() {
+    var header = document.querySelector('.header');
+    console.log(header);
+    if(window.pageYOffset > 400) {
+        header.classList.add("header-fixed");
+    } else {
+        header.classList.remove("header-fixed");
+    }
+};
+
 $(function() {
 
     $("#defaultOpen").get(0).click();
+
+    $(".menu-toggle").click(function(){
+        $(this).toggleClass("active");
+        $(".menu-btn").slideToggle(400);
+    })
 
     // poster frame click event
     $(document).on('click','.js-videoPoster',function(ev) {
